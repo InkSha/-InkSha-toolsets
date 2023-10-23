f2b3ab19ae24943a87854ef04c1a91984da956db
InkSha <x1a010@163.com>
2023-10-23 10:18:37 +0800

    文件模块修改

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -4,142 +4,68 @@
  * @license MIT
  * @author InkSha<git@inksha.com>
  * @created 2023-10-04
- * @updated 2023-10-08
+ * @updated 2023-10-23
  * @version 1.0.0
  */
 
 import fs from 'node:fs'
 import path from 'node:path'
+import crypto from 'node:crypto'
 import { parsePath } from './path'
+import { asyncExec } from './async'
 
-export class Files {
-
-  /**
-  * 创建文件夹
-  * @param dir 文件夹路径
-  * @returns 文件夹是否存在
-  */
-  static mkdir (dir: string): boolean {
-    const parse = path.parse(dir)
-    dir = parse.ext ? parse.dir : dir
-    for (const _path of parsePath(dir)) {
-      if (!Files.fileExist(_path)) fs.mkdirSync(_path)
-    }
-    return Files.fileExist(dir)
+export const mkdir = (dir: string): boolean => {
+  const parse = path.parse(dir)
+  dir = parse.ext ? parse.dir : dir
+  for (const _path of parsePath(dir)) {
+    if (!fileExist(_path)) fs.mkdirSync(_path)
   }
-
-  /**
-   * 异步的创建文件夹
-   * @param dir 文件夹路径
-   * @returns 文件夹是否建立的 Promise
-   */
-  static mkdirAsync (dir: string) {
-    return new Promise<boolean>((resolve, reject) => {
-      try {
-        resolve(this.mkdir(dir))
-      } catch (e) {
-        reject(e)
-      }
-    })
-  }
-
-  /**
-   * 读取文件
-   * @param filePath 文件路径
-   * @param binary 是否是二进制文件
-   * @returns 文件内容
-   */
-  readFile (filePath: string, binary = false): string {
-    return Files.fileExist(filePath)
-      ? filePath
-        ? fs.readFileSync(filePath, {
-          encoding: binary
-            ? 'binary'
-            : 'utf8'
-        })
-        : ''
-      : ''
-  }
-
-  readFileAsync (filePath: string) {
-    return new Promise<string>((resolve, reject) => {
-      try {
-        resolve(this.readFile(filePath))
-      } catch (e) {
-        reject(e)
-      }
-    })
-  }
-
-  /**
-   * 写入文件
-   * @param filePath 文件路径
-   * @param data 写入内容
-   * @returns 文件是否存在
-   */
-  writeFile (filePath: string, data: any, append = true): boolean {
-    if (Files.mkdir(filePath)) {
-      fs.writeFileSync(filePath, data, {
-        flag: append
-          ? 'a+'
-          : 'w+',
-        encoding: typeof data === 'string'
-          ? 'utf-8'
-          : 'binary'
-      })
-    }
-    return Files.fileExist(filePath)
-  }
-
-  writeFileAsync (filePath: string, data: any, append = true) {
-    return new Promise<boolean>((resolve, reject) => {
-      try {
-        resolve(this.writeFile(filePath, data, append))
-      } catch (e) {
-        reject(e)
-      }
-    })
-  }
-
-  /**
-   * 路径是否存在
-   * @param path 路径
-   * @returns 是否存在
-   */
-  static fileExist (path: string): boolean {
-    return fs.existsSync(path)
-  }
-
-  static fileExistAsync (path: string) {
-    return new Promise<boolean>((resolve, reject) => {
-      try {
-        resolve(this.fileExist(path))
-      } catch (e) {
-        reject(e)
-      }
-    })
-  }
-
-  /**
-   * 删除路径
-   * @param path 路径
-   * @returns 路径是否存在
-   */
-  static removeFiles (path: string): boolean {
-    if (this.fileExist(path)) {
-      fs.rmSync(path)
-    }
-    return this.fileExist(path)
-  }
-
-  static removeFilesAsync (path: string) {
-    return new Promise<boolean>((resolve, reject) => {
-      try {
-        resolve(this.removeFiles(path))
-      } catch (e) {
-        reject(e)
-      }
-    })
-  }
-
+  return fileExist(dir)
 }
+
+export const readFile = (filePath: string, binary = false): string => {
+  return fileExist(filePath)
+    ? filePath
+      ? fs.readFileSync(filePath, {
+        encoding: binary
+          ? 'binary'
+          : 'utf8'
+      })
+      : ''
+    : ''
+}
+
+export const writeFile = (filePath: string, data: any, append = true): boolean => {
+  if (mkdir(filePath)) {
+    fs.writeFileSync(filePath, data, {
+      flag: append
+        ? 'a+'
+        : 'w+',
+      encoding: typeof data === 'string'
+        ? 'utf-8'
+        : 'binary'
+    })
+  }
+  return fileExist(filePath)
+}
+
+export const fileExist = (path: string): boolean => fs.existsSync(path)
+
+export const removeFiles = (path: string): boolean => {
+  if (fileExist(path)) fs.rmSync(path)
+  return fileExist(path)
+}
+
+export const mkdirAsync = (dir: string) => asyncExec(() => mkdir(dir))
+
+export const readFileAsync = (filePath: string) => asyncExec(() => readFile(filePath))
+
+export const writeFileAsync = (filePath: string, data: any, append = true) =>
+  asyncExec(() => writeFile(filePath, data, append))
+
+export const fileExistAsync = (path: string) => asyncExec(() => fileExist(path))
+
+export const removeFilesAsync = (path: string) => asyncExec(() => removeFiles(path))
+
+export const computedHash = (buffer: Buffer, Hash?: string): string =>
+  crypto.createHash(Hash ?? 'md5').update(buffer).digest('hex')
