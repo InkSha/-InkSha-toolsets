9533ca607469e3322c18f4f3a3c771d2c425d752
InkSha <x1a010@163.com>
2023-11-28 19:17:18 +0800

    feat: add log module.

--- src/shared/log.ts
+++ src/shared/log.ts
@@ -1,122 +1,156 @@
-import { Time } from './../lib'
+import { Time, toLen, addMiddle } from './../lib'
 
-export enum logColor {
-  italic = '\x1B[3m',
-  underline = '\x1B[4m',
-  reverse = '\x1B[7m',
-  hidden = '\x1B[8m',
+export const wrap = '\n'
+export const indent = '\t'
+export const styleEnd = '\x1b[0m'
 
-  bright = '\x1B[1m',
-  grey = '\x1B[2m',
-  black = '\x1B[30m',
-  red = '\x1B[31m',
-  green = '\x1B[32m',
-  yellow = '\x1B[33m',
-  blue = '\x1B[34m',
-  magenta = '\x1B[35m',
-  cyan = '\x1B[36m',
-  white = '\x1B[37m',
+export const Font = {
+  style: {
+    italic: '\x1B[3m',
+    underline: '\x1B[4m',
+    reverse: '\x1B[7m',
+    hidden: '\x1B[8m',
+  },
+  color: {
+    bright: '\x1B[1m',
+    grey: '\x1B[2m',
+    black: '\x1B[30m',
+    red: '\x1B[31m',
+    green: '\x1B[32m',
+    yellow: '\x1B[33m',
+    blue: '\x1B[34m',
+    magenta: '\x1B[35m',
+    cyan: '\x1B[36m',
+    white: '\x1B[37m'
+  },
+  bg: {
+    black: '\x1B[40m',
+    red: '\x1B[41m',
+    green: '\x1B[42m',
+    yellow: '\x1B[43m',
+    blue: '\x1B[44m',
+    magenta: '\x1B[45m',
+    cyan: '\x1B[46m',
+    white: '\x1B[47m',
+  }
 }
 
-export enum logBackground {
-  black = '\x1B[40m',
-  red = '\x1B[41m',
-  green = '\x1B[42m',
-  yellow = '\x1B[43m',
-  blue = '\x1B[44m',
-  magenta = '\x1B[45m',
-  cyan = '\x1B[46m',
-  white = '\x1B[47m',
+export const logLevel = {
+  info: 'grey',
+  warn: 'yellow',
+  damage: 'red',
+  success: 'green',
+  normal: 'cyan',
+  error: 'magenta'
 }
+
 
 export class Log {
   private text: string[] = []
-  private needClose = false
-  private cols = process.stdout.columns
-  private autoWrap = true
-  private tmpToggle = false
+  private width = process.stdout.columns - 1
+  private line = false
+  private lineArray: string[] = []
+  private endSymbol = ''
 
-  color (color: keyof typeof logColor) {
-    return this.changeColor(logColor[color])
-  }
+  constructor () {}
 
-  background (bg: keyof typeof logBackground) {
-    return this.changeColor(logBackground[bg])
-  }
-
-  changeWrap (tmp = false) {
-    if (tmp) this.tmpToggle = true
-    this.autoWrap = !this.autoWrap
-    return this
-  }
-
-  addText (message: string) {
-    this.text.push(message)
-    if (this.autoWrap) {
-      this.text.push('\n')
-    }
-    if (this.tmpToggle) {
-      this.autoWrap = !this.autoWrap
-      this.tmpToggle = false
+  private cancel (char = '') {
+    if (this.text.slice(-1)[0] === char) {
+      this.text.pop()
     }
     return this
   }
 
-  wrap () {
-    return this.addText('\n')
+  pushText (string = '') {
+    if (this.line) {
+      this.lineArray.push(string)
+    }
+    else this.text.push(string)
+    return this
   }
 
-  print () {
-    console.log(this.text.join(''))
+  indent (add = true) {
+    return add ? this.pushText(indent) : this.cancel(indent)
+  }
+
+  wrap (add = true) {
+    return add ? this.pushText(wrap) : this.cancel(wrap)
+  }
+
+  print (noWrap = true) {
+    console.log(this.text.join(noWrap ? ' ' : wrap))
     return this.reset()
   }
 
   reset () {
     this.text.length = 0
-    return this.changeColor()
+    return this
   }
 
-  warn (message: string) {
-    return this.color('yellow').addText(message).changeColor()
+  head (message: string, split = '-') {
+    return this.pushText(addMiddle(message, split, this.width))
   }
 
-  info (message: string) {
-    return this.color('white').addText(message).changeColor()
+  addTime () {
+    return this.pushText(new Time().timeToString())
   }
 
-  primary (message: string) {
-    return this.color('blue').addText(message).changeColor()
+  addDate () {
+    return this.pushText(new Time().dateToString())
   }
 
-  damage (message: string) {
-    return this.color('red').addText(message).changeColor()
+  addTimer () {
+    return this.addDate().addTime()
   }
 
-  addTimer (split = '-') {
-    const now = new Time()
-    const head = [now.dateToString(), `  ${now.timeToString()}`]
-    const odd = this.cols % 2 !== 0
-    const len = (this.cols / 2) - 10
-    const row = new Array(odd ? Math.floor(len) : len).fill(split).join('')
-    return this.addText(
-      `${row}${head.join(odd ? '' : ' ')}}${row}`
-    )
+  addHeadTime (onlyDate = false) {
+    if (onlyDate) this.addDate()
+    else this.addTimer()
+    return this.head(['[', ...this.text.splice(onlyDate ? -1 : -2), ']'].join(' '))
   }
 
-  split (split = '-') {
-    return this.addText(new Array(this.cols).fill(split).join(''))
+  split (char = '-', len = this.width) {
+    if (this.line) this.endLine()
+    return this.pushText(toLen(char, len))
   }
 
-  private changeColor (color?: string) {
-    if (color) {
-      if (this.needClose) this.text.pop()
-      this.text.push(color)
-      this.needClose = true
-    }
-    else if (this.needClose) {
-      this.text.push('\x1b[0m')
-      this.needClose = false
-    }
+  inline () {
+    if (this.line) this.endLine()
+    this.line = true
+    return this
+  }
+
+  endLine () {
+    this.line = false
+    this.pushText(this.lineArray.join(''))
+    this.lineArray.length = 0
+    return this
+  }
+
+  color (color: keyof typeof Font.color) {
+    return this.pushText(Font.color[color])
+  }
+
+  bg (color: keyof typeof Font.bg) {
+    return this.pushText(Font.bg[color])
+  }
+
+  style (style: keyof typeof Font.style) {
+    return this.pushText(Font.style[style])
+  }
+
+  endFont () {
+    return this.pushText(styleEnd)
+  }
+
+  inner (char = '[]', space = true) {
+    this.endSymbol = `${space ? ' ' : ''}` + char.slice(-1)
+    return this.pushText(char.charAt(0) + `${space ? ' ' : ''}`)
+  }
+
+  endInner () {
+    this.pushText(this.endSymbol)
+    this.endSymbol = ''
     return this
   }
 }
